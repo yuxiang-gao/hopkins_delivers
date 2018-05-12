c67c65e4a28b40ec018be300783f909d66863d7e
DavidGogh <davidgogh96@gmail.com>
2018-05-12 05:32:20 -0400

    fix zed nodelet problem

--- hd_depth/src/obstacle_detection.cpp
+++ hd_depth/src/obstacle_detection.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
   nodelet::M_string remap(ros::names::getRemappings());
   nodelet::V_string nargv;
   std::string nodelet_name = ros::this_node::getName();
-  nodelet.load(nodelet_name, "hd_depth/ObstacleDetectionNodelet", remap, nargv);
+  nodelet.load(nodelet_name, "hd_depth/obstacle_detection_nodelet", remap, nargv);
 
   ros::spin();
 
